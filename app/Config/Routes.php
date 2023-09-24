d91a8e02b2da6f74610d3116b7b091cdd7cb1b9f
royraflesmp <royraflesmatorangpasaribu55.com>
2023-09-24 23:26:45 +0700

    Membuat database-model, validation

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -16,3 +16,4 @@ $routes->get('/profile/(:any)/(:any)/(:any)', [Home::class, 'profile']);
 $routes->get('/user/profile', [UserController::class, 'profile']);
 $routes->get('/user/create', [UserController::class, 'create']);
 $routes->post('/user/store', [UserController::class,'store']);
+$routes->match(['get', 'post'],'/user/store', [UserController::class, 'store']);
--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace App\Controllers;
-
+use App\Models\UserModel;
+use App\Models\KelasModel;
 use App\Controllers\BaseController;
 
 class UserController extends BaseController
 {
+    protected $helpers=['Form'];
     public function index()
     {
         //
@@ -24,14 +26,87 @@ class UserController extends BaseController
 
     public function create()
     {
-        return view('create_user'); 
+        $kelas = [
+            [
+                'id' => 1,
+                'nama_kelas' => 'A'
+            ],
+            [
+                'id' => 2,
+                'nama_kelas' => 'B'
+            ],
+            [
+                'id' => 3,
+                'nama_kelas' => 'C'
+            ],
+            [
+                'id' => 4,
+                'nama_kelas' => 'D'
+            ],
+        ];
+        
+        $data = [
+            'kelas' => $kelas,
+        ];
+
+        return view('create_user', $data); 
     }
 
     public function store(){
         // dd($this->request->getVar());  // test data masuk or no
+
+        // agar tampilan di store kelas terpanggil A, B, C, D
+        $kelasModel = new KelasModel();
+        if($this->request->getVar('kelas') != ''){
+            $kelas_select = $kelasModel->where('id', $this->request->getVar('kelas'))->first();
+            $nama_kelas = $kelas_select['nama_kelas'];
+        }
+        else{
+            $nama_kelas= '';
+        }
+
+        // validasi
+        if(!$this->validate([
+            'nama' => [
+                'rules' => 'required|alpha_space',
+                'errors' => [
+                    'required' => '{field} harus di isi!!',
+                    'alpha_space' => '{field} isi dengan huruf dan spasi!!'
+                ]
+            ],
+            'npm' => [
+                'rules' => 'required|is_unique[user.npm]',
+                'errors' => [
+                    'required' => '{field} harus di isi!!',
+                    'is_unique' => '{field} sudah terdaftar!!'
+                ]
+            ],
+            'kelas' => [
+                'rules' => 'required',
+                'errors' => [
+                'required' => '{field} harus di isi!!',
+                ]
+            ],
+            // 'nama' => 'required|alpha_space',
+            // 'npm' => 'required|is_unique[user.npm]',
+            // 'kelas' => 'required'
+        ])){
+            // dd($validation);
+            session()->setFlashdata('nama_kelas');
+            return redirect()->back()->withInput()->with('nama_kelas', $nama_kelas);
+        }
+       
+        $userModel = new UserModel();
+
+        $userModel->saveUser ([
+            'nama' => $this->request->getVar('nama'),
+            'id_kelas' => $this->request->getVar('kelas'),
+            'npm' => $this->request->getVar('npm'),
+        ]);
+
         $data = [
             'nama' => $this->request->getVar('nama'),
-            'kelas' => $this->request->getVar('kelas'),
+            'kelas' => $nama_kelas,
             'npm' => $this->request->getVar('npm'),
         ];
         return view('profile', $data);
