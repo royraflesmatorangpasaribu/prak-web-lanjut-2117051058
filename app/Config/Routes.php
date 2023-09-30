b3a40f20a6777549760c79964f4f12a66afb914c
royraflesmp <royraflesmatorangpasaribu55.com>
2023-09-30 23:50:32 +0700

    Menambah Read Data

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -17,3 +17,4 @@ $routes->get('/user/profile', [UserController::class, 'profile']);
 $routes->get('/user/create', [UserController::class, 'create']);
 $routes->post('/user/store', [UserController::class,'store']);
 $routes->match(['get', 'post'],'/user/store', [UserController::class, 'store']);
+$routes->get('/user', [UserController::class,'index']);
